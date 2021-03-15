c4aa1fc0a1257ea3b52bab06ff946178b8e96c5e
User <windvogelmickylejohnathan@gmail.com>
2021-03-15 14:37:27 +0200

    updated

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def get_chuck_norris_jokes():
 
-    icon_url = "https://assets.chucknorris.host/img/avatar/chuck-norris.png"
+
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
-    response = requests.get(icon_url).json()
+
+    image_tag = "<img src=" + response['icon_url'] + "alt='Chuck's Images'>"
 
     return "<strong>Random joke from chuck norris: </strong>" + response['value']
 
